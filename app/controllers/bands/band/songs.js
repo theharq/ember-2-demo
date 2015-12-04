f2efab12ae787ccaa00836959413cbf642801be8
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-04 16:48:38 -0500

    Update workflow of band creation

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songsCount: Ember.computed.alias('model.songs.length'),
+  noSongs: Ember.computed.equal('songsCount', 0),
+  songCreationStarted: false,
+  canCreateSong: Ember.computed.or('songCreationStarted', 'songsCount'),
+  isAddButtonDisabled: Ember.computed.empty('title'),
   actions: {
     updateRating(params) {
       let song = params.item,
           rating = params.rating;
       song.set('rating', rating);
-    }
+    },
+    enableSongCreation() {
+      this.set('songCreationStarted', true);
+    },
   }
 });
