d666a3a2fcc2e1b2841a47461f1c702eb972f334
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-04 10:59:57 -0500

    update ember models to user ember-data

--- app/models/band.js
+++ app/models/band.js
@@ -1,11 +1,11 @@
-export default Ember.Object.extend({
-  name: '',
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr(),
+  songs: DS.hasMany('song'),
+
   slug: Ember.computed('name', function() {
     return this.get('name').dasherize();
-  }),
-  setupSongs: Ember.on('init', function() {
-    if (!this.get('songs')) {
-      this.set('songs', []);
-    }
   })
 });
--- app/models/song.js
+++ app/models/song.js
@@ -1,5 +1,7 @@
-export default Ember.Object.extend({
-  title: '',
-  rating: 0,
-  band: null
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  rating: DS.attr('number'),
+  band: DS.belongsTo('band')
 });
