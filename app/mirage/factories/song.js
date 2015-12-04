6a30b823c3903e5a4410a2f8d96d4c66c25cf6e7
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-04 15:22:35 -0500

    Fix faker call to generate random rating

--- app/mirage/factories/song.js
+++ app/mirage/factories/song.js
@@ -2,5 +2,5 @@ import Mirage, {faker} from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
   title(i) { return `Title ${i}`; },
-  rating() { return faker.list.random(1, 2, 3, 4, 5); }
+  rating() { return faker.list.random(1, 2, 3, 4, 5)(); }
 });
