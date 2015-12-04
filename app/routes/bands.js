bdfeac8385f9a89eeb5cdbfb60a76efbdf6aa778
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-04 13:16:51 -0500

    Form to add new band, validate empty input field

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('band');
+  },
+
+  actions: {
+    createBand(){
+      var name = this.get('controller').get('name');
+      alert(name);
+      this.get('controller').set('name', '');
+    }
   }
 });
