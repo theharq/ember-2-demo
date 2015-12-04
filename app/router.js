4c07d21215501fab80e566c1bfd4ed555f41a71f
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-04 17:50:41 -0500

    Add tabs to details and songs pages for bands

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ Router.map(function() {
   this.route('bands', function() {
     this.route('band', {path: ':id'}, function() {
       this.route('songs');
+      this.route('details');
+      this.route('bands');
     });
   });
 });
---
+++ app/routes/bands/band/index.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  afterModel(band) {
+    if (Ember.isEmpty(band.get('description'))) {
+      this.transitionTo('bands.band.songs');
+    } else {
+      this.transitionTo('bands.band.details');
+    };
+  }
+});
