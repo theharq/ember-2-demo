935635ae2c62fc6fb9d2f0d3ca5242313a4da7a8
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-04 18:05:25 -0500

    Example of aborting transition

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.modelFor('bands.band');
+  },
+
+  actions: {
+    willTransition(transition) {
+      if (this.get('controller.isEditing')) {
+        let leave = window.confirm("You have unsaved changes. Are you sure you want to leave?");
+        if (leave) {
+          this.set('controller.isEditing', false);
+        } else {
+          transition.abort();
+        }
+      }
+    }
   }
 });
