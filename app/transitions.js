31adcdfee217ad9d298cf9ea0db740ec18652893
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-04 19:09:49 -0500

    add basic transitions

---
+++ app/transitions.js
@@ -0,0 +1,14 @@
+export default function(){
+  this.transition(
+    this.fromRoute('bands.band.songs'),
+    this.toRoute('bands.band.details'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  );
+
+  this.transition(
+    this.hasClass('band-description'),
+    this.toValue(false),
+    this.use('fade', { duration: 500 })
+  );
+}
