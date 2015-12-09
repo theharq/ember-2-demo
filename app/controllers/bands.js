5874930c4384e2e6c5814f0d45ec482b67e9e108
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-08 21:41:04 -0500

    Replace CP with ember computed macro

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -2,7 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   name: '',
-  isAddButtonDisabled: Ember.computed('name', function() {
-    return Ember.isEmpty(this.get('name'));
-  })
+  isAddButtonDisabled: Ember.computed.empty('name')
 });
