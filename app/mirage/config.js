c8b09573081248d97493e762e7508982b5211556
Hildebrando Rueda <harq.soft@gmail.com>
2015-12-08 23:39:21 -0500

    jshint complaining

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,6 +1,6 @@
 export default function() {
 
-  this.get('/bands', function(db, request) {
+  this.get('/bands', function(db) {
     let data = {};
     data = db.bands.map((attrs) => {
       let songs = db.songs.where({band_id: attrs.id});
@@ -19,7 +19,7 @@ export default function() {
       return band;
     });
     return { data };
-  }),
+  });
 
   this.get('/bands/:id', function(db, request) {
     let id = request.params.id;
@@ -40,7 +40,7 @@ export default function() {
         }
       }
     };
-  }),
+  });
 
   this.get('/songs/:id', function(db, request) {
     let id = request.params.id;
@@ -52,29 +52,29 @@ export default function() {
         attributes: db.songs.find(id)
       }
     };
-  }),
+  });
 
   this.post('/bands', (db, request) => {
     let payload = JSON.parse(request.requestBody);
     let band = db.bands.insert(payload.data.attributes);
     return {data: { type: 'bands', id: band.id, attributes: band}};
-  }),
+  });
 
   this.post('/songs', (db, request) => {
     let payload = JSON.parse(request.requestBody);
     let song = db.songs.insert(payload.data.attributes);
     return {data: { type: 'songs', id: song.id, attributes: song}};
-  }),
+  });
 
   this.patch('/bands/:id', (db, request) => {
     let payload = JSON.parse(request.requestBody);
     let band = db.bands.update(request.params.id, payload.data.attributes);
     return {data: { type: 'bands', id: band.id, attributes: band}};
-  }),
+  });
 
   this.patch('/songs/:id', (db, request) => {
     let payload = JSON.parse(request.requestBody);
     let song = db.songs.update(request.params.id, payload.data.attributes);
     return {data: { type: 'songs', id: song.id, attributes: song}};
-  })
+  });
 }
